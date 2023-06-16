0949532a28fc706b142fa7df0d9e3a547ff0e0a0
YTW7 <91966435+YTW7@users.noreply.github.com>
2023-06-16 18:25:18 +0000

    added redux

---
+++ crud/pages/_app.js
@@ -0,0 +1,16 @@
+import '@/styles/globals.css'
+import { QueryClientProvider, QueryClient } from 'react-query';
+
+const queryClient = new QueryClient();
+
+export default function App({ Component, pageProps }) {
+  
+  return (
+  <>
+  <QueryClientProvider client={queryClient}>
+  <Component {...pageProps} />
+  </QueryClientProvider>
+  </>
+  )
+
+}
